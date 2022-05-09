f87580c8de0ccc6af3870dd282e8e454dbe9f292
nasirdiu <nasir.joypurhat@gmail.com>
2022-05-10 01:17:32 +0600

    service

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Banner from "../Banner/Banner";
 import Info from "../Info/Info";
+import BannerService from "../Services/BannerService";
+import Services from "../Services/Services";
 
 const Home = () => {
   return (
-    <div>
+    <div className="px-12">
       <Banner></Banner>
       <Info></Info>
+      <Services></Services>
+      <BannerService></BannerService>
     </div>
   );
 };
---
+++ src/Pages/Home/Services/Service.js
@@ -0,0 +1,24 @@
+import React from "react";
+import BannerService from "./BannerService";
+
+const Service = ({service}) => {
+  return (
+    <div class="card lg:max-w-lg bg-base-100 shadow-xl">
+      <figure class="px-10 pt-10">
+        <img width={116} height={115}
+          src={service.img}
+          alt="Shoes"
+          class="rounded-xl"
+        />
+      </figure>
+      <div class="card-body items-center text-center">
+        <h2 class="card-title">{service.name}</h2>
+        <p>{service.description}</p>
+        
+      </div>
+      
+    </div>
+  );
+};
+
+export default Service;
